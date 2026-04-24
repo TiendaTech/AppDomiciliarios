9d240d3affcb9bd8f15855ed8d9a4a6b2da74eab
Jhon <tiendatech26@gmail.com>
2026-04-24 13:32:39 -0500

    Ok

---
+++ sw.js
@@ -0,0 +1,30 @@
+const CACHE_NAME = 'domiapp-v2';
+const urlsToCache = [
+  'domi.html',
+  'LOGO DOMIFASTER.png',
+  'manifest.json'
+];
+
+self.addEventListener('install', event => {
+  self.skipWaiting();
+  event.waitUntil(
+    caches.open(CACHE_NAME).then(cache => cache.addAll(urlsToCache))
+  );
+});
+
+self.addEventListener('activate', event => {
+  event.waitUntil(
+    caches.keys().then(keys => Promise.all(
+      keys.map(key => {
+        if (key !== CACHE_NAME) return caches.delete(key);
+      })
+    ))
+  );
+});
+
+self.addEventListener('fetch', event => {
+  // Estrategia Network First: Priorizar internet para tener los datos reales
+  event.respondWith(
+    fetch(event.request).catch(() => caches.match(event.request))
+  );
+});
